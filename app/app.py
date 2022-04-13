9409a8d8c1c44663be84fe4c50dfb8089b449117
David <d.samakhavets@biquants.com>
2022-04-13 17:56:33 +0100

    Database init function

--- app/app.py
+++ app/app.py
@@ -1,19 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
-from db.config import engine, Base
 from routers import forecast_router
 
 app = FastAPI()
 app.include_router(forecast_router.router)
-
-
-@app.on_event("startup")
-async def startup():
-    # create db tables
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
 
 
 if __name__ == '__main__':
